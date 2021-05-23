e88cb5293a994b744e5faad0f267ceaacb44a1ae
Pankaj100ni <59951275+Pankaj100ni@users.noreply.github.com>
2021-05-23 20:12:53 +0530

    Add files via upload

---
+++ pankaj-191919-program-8.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+using namespace std;
+
+class Shape
+{
+    public:
+    virtual void numberOfSides() = 0 ;
+
+};
+class Trapezoid:public Shape
+{
+    public:
+      void numberOfSides()
+        {
+               cout<<"number of sides in trapezoid : 4 "<<endl;
+        }
+};
+class Triangle:public Shape
+{
+    public:
+
+     void numberOfSides()
+        {
+               cout<<"number of sides in triangle : 3 "<<endl;
+        }
+
+};
+class Hexagon:public Shape
+{
+    public:
+     void numberOfSides()
+        {
+               cout<<"number of sides in hexagon : 6 "<<endl;
+        }
+
+};
+
+int main()
+{
+    Trapezoid t;
+    Triangle tr;
+    Hexagon h;
+    t.numberOfSides();
+    tr.numberOfSides();
+    h.numberOfSides();
+    
+     return 0;
+}
